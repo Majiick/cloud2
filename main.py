8bbb0a2fbc4981a5fec6a6eea5ea560965c30285
Zan Smirnov <c15437072@mydit.ie>
2017-12-01 14:53:16 +0000

    Initial

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ import subprocess
 app = Flask(__name__)
 
 
+def json_error(err: str):
+    return json.dumps({'error': err})
+
+
 @app.route('/containers', methods=['GET'])
 def containers_index():
     """
@@ -34,17 +38,103 @@ def containers_index():
             output = docker('ps', '-a')
             resp = json.dumps(parse_docker_ps(output))
     except subprocess.CalledProcessError as e:
-        return str(e), 403
+        return json_error(str(e))
 
     return Response(response=resp, mimetype="application/json")
 
 
+@app.route('/containers/<id_>', methods=['GET'])
+def containers_specific(id_):
+    output = docker('inspect', str(id_))
+    resp = output
 
+    return Response(response=resp, mimetype="application/json")
+
+
+@app.route('/containers/<id_>/logs', methods=['GET'])
+def containers_specific_logs(id_):
+    output = docker('logs', str(id_))
+    resp = output
+
+    return Response(response=resp, mimetype="application/json")
+
+
+@app.route('/services', methods=['GET'])
+def services():
+    output = docker('service' 'ps')
+    resp = output
+
+    return Response(response=resp, mimetype="application/json")
+
+
+@app.route('/nodes', methods=['GET'])
+def nodes():
+    output = docker('node' 'ls')
+    resp = output
+
+    return Response(response=resp, mimetype="application/json")
+
+
+@app.route('/containers', methods=['POST'])
+def containers_create():
+    # Create a container with ?image=<imagename>
+    output = docker('create', str(request.args.get('image')))
+    resp = output
+
+    return Response(response=resp, mimetype="application/json")
+
+
+'''
+@app.route('/containers/<id_>', methods=['PATCH'])
+def containers_patch():
+    output = docker('create' 'hello-world')
+    resp = output
+
+    return Response(response=resp, mimetype="application/json")
+'''
+
+
+@app.route('/containers/<id_>', methods=['DELETE'])
+def containers_delete_id(id_):
+    output = docker('rm', str(id_))
+    resp = output
+
+    return Response(response=resp, mimetype="application/json")
+
+
+@app.route('/images', methods=['GET'])
+def images_get():
+    output = docker('image', 'ls')
+    resp = output
+
+    return Response(response=resp, mimetype="application/json")
+
+
+@app.route('/containers', methods=['DELETE'])
+def containers_delete():
+    output = docker('rm', '$(docker ps -a -q)')
+    resp = output
+
+    return Response(response=resp, mimetype="application/json")
+
+
+@app.route('/images', methods=['DELETE'])
+def images_delete():
+    resp = docker('rmi', '$(docker images -q)')
+
+    return Response(response=resp, mimetype="application/json")
+
+
+@app.route('/images/<id_>', methods=['DELETE'])
+def images_delete_id(id_):
+    resp = docker('rmi', str(id_))
+
+    return Response(response=resp, mimetype="application/json")
 
 
 def docker(*args):
     try:
-        completed_process = subprocess.run(['docker'].extend(args), shell=True, check=True, stderr=subprocess.STDOUT)
+        completed_process = subprocess.run(['docker'] + list(args), shell=True, check=True, stderr=subprocess.STDOUT)
     except subprocess.CalledProcessError as e:
         print(e)
         raise
@@ -53,4 +143,4 @@ def docker(*args):
 
 
 if __name__ == '__main__':
-    pass
+    app.run()
